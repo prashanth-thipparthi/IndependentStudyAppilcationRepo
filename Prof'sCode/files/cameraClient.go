e1ccafa2e4f43a51ef50a2dd13ad034e4256171d
prashanth thipparthi <prth3635@colorado.edu>
2020-04-06 14:12:01 -0600

    updated code

--- Prof'sCode/files/cameraClient.go
+++ Prof'sCode/files/cameraClient.go
@@ -7,29 +7,22 @@ import (
         "os"
         "strconv"
         "strings"
-//        "bufio"
 )
 
 import (
         //      "bytes"
 //        "errors"
-//        "fmt"
         "time"
         "image/color"
         "gocv.io/x/gocv"
-
-//        "github.com/edgexfoundry/app-functions-sdk-go/pkg/transforms"
-
- //       "github.com/edgexfoundry/go-mod-core-contracts/models"
-
- //       "github.com/edgexfoundry/app-functions-sdk-go/appcontext"
- //       "github.com/edgexfoundry/app-functions-sdk-go/appsdk"
- //       "github.com/almutawm/netConn"
 )
 
 const (
+        S_CONN_HOST = "0.0.0.0"
+        S_CONN_PORT = "8181"
+        S_CONN_TYPE = "tcp"
         CONN_HOST = "0.0.0.0"
-        CONN_PORT = "8181"
+        CONN_PORT = "8180"
         CONN_TYPE = "tcp"
         BUFFERSIZE = 1024
 )
@@ -40,7 +33,7 @@ func Check(e error, s string) {
         }
 }
 
-func faceDetection(img gocv.Mat) {
+func faceDetection(img gocv.Mat)(string) {
 
         defer img.Close()
 
@@ -56,7 +49,7 @@ func faceDetection(img gocv.Mat) {
         fmt.Printf("one")
         if !classifier.Load(xmlFile) {
                 fmt.Printf("Error reading cascade file: %v\n", xmlFile)
-                return
+                return ""
         }
         fmt.Printf("two")
         // detect faces
@@ -75,9 +68,10 @@ func faceDetection(img gocv.Mat) {
         b := gocv.IMWrite(fileName, img)
         if (!b) {
                 fmt.Println("Writing Mat to file failed")
-                return
+                return ""
         }
         fmt.Println("Just saved " + fileName)
+        return fileName
  //       netConn.ConnectToSend(conn_host, conn_port, "FILE0", fileName)
 }
 
@@ -116,9 +110,86 @@ func RecvFile(conn net.Conn, path string) string {
         fmt.Println("Received file: ", fileName,", bytes: ", receivedBytes)
         return fileName
 }
-func main() {
+
+func ConnectToSend(conn_host string, conn_port string, typeOfData string, data string) {
+
+        conn, err := net.Dial(CONN_TYPE, conn_host+":"+conn_port)
+
+        Check(err, "Unable to to establish a connection")
+
+        defer conn.Close()
+
+        fmt.Println("Connected to servere ")
+
+        if typeOfData == "FILE0" {
+                SendFile(conn, data)
+        } else {
+                if len(typeOfData) == 5 {
+                        SendText(conn, typeOfData, data)
+                } else {
+                        fmt.Println("Nothing was sent, typeOfData must be 5 characters long")
+                }
+        }
+
+        fmt.Println("Closing connection!")
+}
+
+func SendText(conn net.Conn, typeOfData string, data string) {
+        defer conn.Close()
+        conn.Write([]byte(typeOfData))
+        conn.Write([]byte(data+";;;;"))
+}
+
+func fillString(retunString string, toLength int) string {
+        for {
+                lengtString := len(retunString)
+                if lengtString < toLength {
+                        retunString = retunString + ":"
+                        continue
+                }
+                break
+        }
+        return retunString
+}
+func SendFile(conn net.Conn, filename string) {
+
+        defer conn.Close()
+        fmt.Println("opening file:",filename)
+        file, err := os.Open(filename)
+
+        Check(err, "Unable to open file, exiting")
+
+        fileInfo, err := file.Stat()
+        Check(err, "Unable to get file Stat, exiting")
+        
+        fileSize := fillString(strconv.FormatInt(fileInfo.Size(), 10), 10)
+
+        conn.Write([]byte(fileSize))
+        fileName := fillString(fileInfo.Name(), 32)
+
+        //conn.Write([]byte("FILE0"))
+        conn.Write([]byte(fileSize))
+        conn.Write([]byte(fileName))
+
+        sendBuffer := make([]byte, BUFFERSIZE)
+        fmt.Println("Start sending file!")
+        nBytes := 0
+        for {
+                _, err = file.Read(sendBuffer)
+                if err == io.EOF {
+                        fmt.Println("reached end of file")
+                        break
+                }
+                n, _ := conn.Write(sendBuffer)
+                nBytes += n
+        }
+        fmt.Println("File: ", fileName ," has been sent, file size: ", fileSize , ", number of bytes sent: ", nBytes)
+}
+
+func handleRequest(clientCon net.Conn) {
         var data string 
-        conn, err := net.Dial(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+        var processedFileName string 
+        conn, err := net.Dial(S_CONN_TYPE, S_CONN_HOST+":"+S_CONN_PORT)
         Check(err, "Unable to create file")
         data = RecvFile(conn,"/home/pi/")
         fmt.Println("Received file: ",data)
@@ -132,7 +203,26 @@ func main() {
         } else {
            fmt.Println("About to detect face")
            //go faceDetection(img)
-           faceDetection(img) 
+           processedFileName = faceDetection(img) 
+        }
+        SendFile(clientCon, processedFileName)
+}
+
+func main() {
+        l, err := net.Listen(CONN_TYPE, CONN_HOST+":"+CONN_PORT)
+        Check(err, "Unable to listen on port ...")
+        i:=0
+        // Close the listener when the application closes.
+        defer l.Close()
+        fmt.Println("Listening on " + CONN_HOST + ":" + CONN_PORT)
+        for {
+                i+=1
+                // Listen for an incoming connection.
+                conn, err := l.Accept()
+                Check(err, "Error when trying to accept")
+                fmt.Println("Accepted connection ", i)
+                // Handle connections
+                handleRequest(conn)
         }
 
 }
