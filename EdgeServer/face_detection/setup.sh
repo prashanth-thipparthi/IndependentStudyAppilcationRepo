4f298445e09f6d949e90cf1e2cf3be82fcc91c5f
prashanth thipparthi <prashanth.reddyt09@gmail.com>
2020-04-22 07:50:30 -0600

    docker file changes in face detection

--- EdgeServer/face_detection/setup.sh
+++ EdgeServer/face_detection/setup.sh
@@ -2,4 +2,4 @@ apt-get install git
 cd /
 git clone git@github.com:prashanth-thipparthi/IndependentStudyAppilcationRepo.git
 pwd
-cd /IndependentStudyAppilcationRepo/ProfCode/files/
+cd /IndependentStudyAppilcationRepo/EdgeServer/face_detection/
